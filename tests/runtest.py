4a6288a87b5aafa64c7f0724558278f78b9df22b
Eric Berquist <eric.berquist@gmail.com>
2022-10-07 17:30:05 -0400

    blacken runtest.py

--- tests/runtest.py
+++ tests/runtest.py
@@ -4,41 +4,41 @@
 #                                                              #
 ################################################################
 
-#LOVCI semi-automated test suite
+# LOVCI semi-automated test suite
 
-#Modules
+# Modules
 import subprocess
 import sys
 import os
 from pathlib import Path
 import argparse
 
-#Classes
 class ClrSet:
-  #Unicode colors
-  Norm = '\033[0m'
-  Red = '\033[91m'
-  Bold = '\033[1m'
-  Green = '\033[92m'
-  Blue = '\033[94m'
-  Cyan = '\033[36m'
-  #Set colors
-  TFail = Bold+Red #Highlight failed tests
-  TPass = Bold+Green #Highlight passed tests
-  Reset = Norm #Reset to defaults
+    # Unicode colors
+    Norm = "\033[0m"
+    Red = "\033[91m"
+    Bold = "\033[1m"
+    Green = "\033[92m"
+    Blue = "\033[94m"
+    Cyan = "\033[36m"
+    # Set colors
+    TFail = Bold + Red  # Highlight failed tests
+    TPass = Bold + Green  # Highlight passed tests
+    Reset = Norm  # Reset to defaults
 
-#Print title
-line = '\n'
+
+# Print title
+line = "\n"
 line += "****************************************************************"
-line += '\n'
+line += "\n"
 line += "*                                                              *"
-line += '\n'
+line += "\n"
 line += "* LOVCI: Ladder Operator Vibrational Configuration Interaction *"
-line += '\n'
+line += "\n"
 line += "*                                                              *"
-line += '\n'
+line += "\n"
 line += "****************************************************************"
-line += '\n'
+line += "\n"
 print(line)
 
 parser = argparse.ArgumentParser()
@@ -48,16 +48,16 @@ args = parser.parse_args()
 lovci_path = args.lovci_path.resolve()
 assert lovci_path.exists()
 
-Ncpus = subprocess.check_output("echo ${OMP_NUM_THREADS}",shell=True).decode("utf-8").strip()
+Ncpus = subprocess.check_output("echo ${OMP_NUM_THREADS}", shell=True).decode("utf-8").strip()
 if not Ncpus:
     Ncpus = "1"
 
-#Print output
+# Print output
 result = "Running tests with "
 result += Ncpus
 result += " CPUs..."
-result += '\n'
-result += '\n'
+result += "\n"
+result += "\n"
 result += "Test results:"
 print(result)
 
@@ -67,23 +67,23 @@ os.chdir(args.test_case)
 result = ""
 ZPE = ""
 RunTime = ""
-ret = subprocess.run(f"{lovci_path} -n {Ncpus} -i input -o Spect.txt > Log.txt",shell=True)
+ret = subprocess.run(f"{lovci_path} -n {Ncpus} -i input -o Spect.txt > Log.txt", shell=True)
 data = Path("Log.txt").read_text().splitlines()
 ref = Path("ref").read_text().strip()
 for line in data:
-  line = line.strip()
-  line = line.split()
-  if (len(line) > 3):
-    if (line[0] == "Zero-point"):
-      ZPE = line[2]
-    if ((line[0] == "Run") and (line[1] == "time:")):
-      RunTime = line[2]+" "+line[3]
-result += " Harmonic oscillator:      "
-if (ZPE == ref):
-  result += ClrSet.TPass+"Pass"+ClrSet.Reset+", "
+    line = line.strip()
+    line = line.split()
+    if len(line) > 3:
+        if line[0] == "Zero-point":
+            ZPE = line[2]
+        if (line[0] == "Run") and (line[1] == "time:"):
+            RunTime = line[2] + " " + line[3]
+result += f"{args.test_case}: "
+if ZPE == ref:
+    result += ClrSet.TPass + "Pass" + ClrSet.Reset + ", "
 else:
-  result += ClrSet.TFail+"Fail"+ClrSet.Reset+", "
-  rv += 1
+    result += ClrSet.TFail + "Fail" + ClrSet.Reset + ", "
+    rv += 1
 result += RunTime
 print(result)
 # cmd = "rm -f Spect.txt Log.txt"
